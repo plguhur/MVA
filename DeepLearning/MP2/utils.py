31284b9b8f59f5bd09576ab893f7f51fd62bd52f
Pierre-Louis Guhur <pierre-louis.guhur@mv-robot.com>
2019-01-10 02:23:14 +0100

    add part 2 and 3

--- DeepLearning/MP2/utils.py
+++ DeepLearning/MP2/utils.py
@@ -1,12 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def decode_with_labels(path):
     with open(path, 'r') as f:
         lines = f.readlines()
-    label = [int(line[0]) for line in lines] 
-    words = [np.fromstring(line[2:], sep=' ') for line in lines]
+    label = [int(line[0]) for line in lines]
+    words = [line[2:].split() for line in lines]
     return words, label
 
 def decode_without_labels(path):
     with open(path, 'r') as f:
         lines = f.readlines()
-    words = [np.fromstring(line[2:], sep=' ') for line in lines]
+    words = [line[2:].split() for line in lines]
     return words
+
+
+def plot_history(history):
+    if 'acc' in history.history:
+        plt.figure(figsize=(20,10))
+        plt.subplot(121)
+        plt.plot(history.history['acc'])
+        plt.plot(history.history['val_acc'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.grid(True)
+
+        plt.subplot(122)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.grid(True)
+    plt.show()
